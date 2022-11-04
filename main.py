71edbd200dee017b93dc00cd4a6d77b619e7a39e
BennyNottonsonSchool <112336374+BennyNottonsonSchool@users.noreply.github.com>
2022-11-04 16:49:32 -0700

    System change on playlist creation
    Modified so playlists are edited rather than remade, removing the need for image copying. Also fixed some typehints.

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-import base64
-from io import BytesIO
+import pandas
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 import requests
 from PIL import Image
 
-# IDS NEED TO BE INPUT BY THE USER, THIS SHOULD BE DONE THROUGH CREATING A SPOTIFY DEVELOPER APPLICATION
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="",
-                                               client_secret="",
-                                               redirect_uri="",
-                                               scope=""))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="37c0cd0d045d4728995a345cd3de949a",
+                                               client_secret="66cf7cbac6b647168befe4d2db016cd2",
+                                               redirect_uri="http://example.com",
+                                               scope="user-library-modify playlist-modify-public ugc-image-upload playlist-modify-private"))
 user_id = sp.current_user()['id']
 playlists = sp.current_user_playlists()
 
 
-def append_row(df:pd.DataFrame, row:list) -> None:
+def append_row(df: pandas.DataFrame, row: list) -> None:
     df.loc[len(df.index)] = row
 
 
-def normalize_color(rgb:tuple) -> tuple:
+def normalize_color(rgb: tuple) -> tuple:
     return tuple([x / 255 for x in rgb])
 
 
-def rgb_to_hsY(rgb:tuple) -> tuple:
+def rgb_to_hsY(rgb: tuple) -> tuple:
     r, g, b = rgb
     maxc = max(r, g, b)
     minc = min(r, g, b)
@@ -47,20 +45,19 @@ def rgb_to_hsY(rgb:tuple) -> tuple:
         h = 4.0 + gc - rc
     h = int((h / 6.0) % 1.0 * 360.0)
     Y = 0.2126 * r ** 2.2 + 0.7152 * g ** 2.2 + 0.0722 * b ** 2.2
-
     return h, s, Y
 
 
-def is_vivid(s:float, Y:float) -> bool:
+def is_vivid(s: int, Y: int) -> bool:
     return s > 0.15 and 0.18 < Y < 0.95
 
 
-def get_rainbow_band(hue:int, band_deg:int) -> int:
+def get_rainbow_band(hue: int, band_deg: int) -> int:
     rb_hue = (hue + 30) % 360
     return rb_hue // band_deg
 
 
-def get_image_rainbow_bands_and_perceived_brightness(image:Image, band_deg:int) -> tuple:
+def get_image_rainbow_bands_and_perceived_brightness(image: Image, band_deg: int) -> tuple:
     pixels = image.convert('RGB').getdata()
     band_cnt = 360 // band_deg
     all_bands = dict.fromkeys(range(band_cnt), 0)
@@ -89,21 +86,21 @@ def get_image_rainbow_bands_and_perceived_brightness(image:Image, band_deg:int) 
     return bands, perceived_luminance, vividness
 
 
-def get_primary_band(bands:dict) -> int:
+def get_primary_band(bands: dict) -> int:
     return max(bands, key=bands.get)
 
 
-def getPlaylistID(playlistName:str) -> str:
-    for lists in playlists['items']:
-        if lists['name'] == playlistName:
-            return lists['id']
+def getPlaylistID(playlistName: str) -> int or None:
+    for pLists in playlists['items']:
+        if pLists['name'] == playlistName:
+            return pLists['id']
     return None
 
 
-def sortPlaylist(playlistName:str) -> None:
-    source_playlist_id = f'spotify:user:spotifycharts:playlist:{getPlaylistID(playlistName)}'
+def sortPlaylist(playlistName: str) -> None:
+    playlistID = getPlaylistID(playlistName)
+    source_playlist_id = f'spotify:user:spotifycharts:playlist:{playlistID}'
     pl_results = sp.playlist(source_playlist_id, fields='name,tracks.total')
-    playlist_name = pl_results['name']
     playlist_length = pl_results['tracks']['total']
     offset = 0
     playlist_items = []
@@ -119,35 +116,27 @@ def sortPlaylist(playlistName:str) -> None:
         track_id = track['id']
         track_number = track['track_number']
         cover_image_url = track['album']['images'][-1]['url']
-        track_image = Image.open(requests.get(cover_image_url, stream=True).raw).resize((10, 10))
+        track_image = Image.open(requests.get(cover_image_url, stream=True).raw).resize((16, 16))
         bands, Y, vividness = get_image_rainbow_bands_and_perceived_brightness(track_image, band_deg=30)
         primary_band = get_primary_band(bands)
         append_row(df, [track_id, primary_band, Y, vividness, track_number, cover_image_url])
     df = df.sort_values(by=['band', 'Y', 'track_number'])
     sorted_track_ids = df['track_id'].tolist()
-    newPlaylist = sp.user_playlist_create(user=user_id, public=True,
-                                          name=playlist_name)
-
-    playlist_image_url = sp.playlist(source_playlist_id, fields='images')['images'][0]['url']
-    playlist_image = Image.open(requests.get(playlist_image_url, stream=True).raw)
-    buffered = BytesIO()
-    playlist_image.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue())
-    sp.playlist_upload_cover_image(newPlaylist['id'], img_str)
-
+    sp.playlist_remove_all_occurrences_of_items(playlist_id=playlistID, items=sorted_track_ids)
     offset = 0
     while offset < playlist_length:
-        sp.playlist_add_items(playlist_id=newPlaylist['id'], items=sorted_track_ids[offset:offset + 100])
+        sp.playlist_add_items(playlist_id=source_playlist_id, items=sorted_track_ids[offset:offset + 100])
         offset += 100
 
 
-for lists in playlists['items']:
-    print(lists['name'])
-choice = input("Enter playlist name: ")
+if __name__ == '__main__':
+    for lists in playlists['items']:
+        print(lists['name'])
+    choice = input("Enter playlist name: ")
 
-for playlist in playlists['items']:
-    if playlist['name'] == choice:
-        print("Sorting...")
-        sortPlaylist(choice)
-        print("Done!")
-        break
+    for playlist in playlists['items']:
+        if playlist['name'] == choice:
+            print("Sorting...")
+            sortPlaylist(choice)
+            print("Done!")
+            break
