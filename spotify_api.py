a2894d3c77dbf42efe1344d3b596fda848527ccc
Benny Nottonson <112336374+BennyNottonsonSchool@users.noreply.github.com>
2023-04-24 20:45:06 -0700

    Formatting

--- spotify_api.py
+++ spotify_api.py
@@ -13,13 +13,19 @@ from urllib3.util import Retry
 
 class FastHTTPClient:
     """This class is used to make requests faster by using a session and retrying on failure"""
-    def __init__(self, retries=3, backoff_factor=0.3, status_force_list=(500, 502, 504)):
+
+    def __init__(
+        self, retries=3, backoff_factor=0.3, status_force_list=(500, 502, 504)
+    ):
         self.session = Session()
-        retries = Retry(total=retries, backoff_factor=backoff_factor,
-                        status_forcelist=status_force_list)
+        retries = Retry(
+            total=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=status_force_list,
+        )
         adapter = HTTPAdapter(max_retries=retries)
-        self.session.mount('https://', adapter)
-        self.session.mount('https://', adapter)
+        self.session.mount("https://", adapter)
+        self.session.mount("https://", adapter)
 
     def get(self, url, params=None, **kwargs):
         """Calls a get request with a url and params"""
@@ -47,7 +53,9 @@ class SpotifyAPIManager:
     """This class is used to authenticate and manage the Spotify API"""
 
     # pylint: disable=too-many-instance-attributes
-    def __init__(self, client_id: str, client_secret: str, redirect_uri: str, scope: str) -> None:
+    def __init__(
+        self, client_id: str, client_secret: str, redirect_uri: str, scope: str
+    ) -> None:
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
@@ -59,7 +67,7 @@ class SpotifyAPIManager:
         self.api_url = "https://api.spotify.com/v1/"
         self.cache_path = path.join(getcwd(), ".cache")
         if path.exists(self.cache_path):
-            with open(self.cache_path, encoding='utf-8') as file:
+            with open(self.cache_path, encoding="utf-8") as file:
                 cache = load(file)
                 if cache["expires_at"] > datetime.now().timestamp():
                     self.token = cache["access_token"]
@@ -87,68 +95,61 @@ class SpotifyAPIManager:
             "expires_at": self.token_expires.timestamp(),
             "authorization_code": self.code,
         }
-        with open(self.cache_path, "w", encoding='utf-8') as file:
+        with open(self.cache_path, "w", encoding="utf-8") as file:
             dump(cache, file)
 
     def get_authorize_url(self) -> str:
         """Returns the url to authorize the app"""
         params = {
-            'client_id': self.client_id,
-            'response_type': 'code',
-            'redirect_uri': self.redirect_uri,
-            'scope': self.scope
+            "client_id": self.client_id,
+            "response_type": "code",
+            "redirect_uri": self.redirect_uri,
+            "scope": self.scope,
         }
-        url = 'https://accounts.spotify.com/authorize?' + urlencode(params)
+        url = "https://accounts.spotify.com/authorize?" + urlencode(params)
         return url
 
     def get_authorization_code(self) -> str:
         """Returns the authorization code from the url"""
-        base_url = 'https://accounts.spotify.com/authorize'
+        base_url = "https://accounts.spotify.com/authorize"
         query_params = {
-            'response_type': 'code',
-            'client_id': self.client_id,
-            'redirect_uri': self.redirect_uri,
-            'scope': self.scope
+            "response_type": "code",
+            "client_id": self.client_id,
+            "redirect_uri": self.redirect_uri,
+            "scope": self.scope,
         }
-        url = base_url + '?' + urlencode(query_params)
+        url = base_url + "?" + urlencode(query_params)
         open_browser(url)
         code = input("Enter the full url: ")
-        code = code.split('=')[1]
+        code = code.split("=")[1]
         return code
 
     def get_access_token(self) -> str:
         """Returns the access token"""
         if self.token and datetime.now() < self.token_expires:
             return self.token
-        headers = {
-            'Authorization': f'Basic {self.get_auth_header()}'
-        }
+        headers = {"Authorization": f"Basic {self.get_auth_header()}"}
         data = {
-            'grant_type': 'authorization_code',
-            'code': self.code,
-            'redirect_uri': self.redirect_uri
+            "grant_type": "authorization_code",
+            "code": self.code,
+            "redirect_uri": self.redirect_uri,
         }
         response = post(self.token_url, headers=headers, data=data, timeout=5)
         if response.status_code == 200:
-            self.token = response.json()['access_token']
-            expires_in = response.json()['expires_in']
+            self.token = response.json()["access_token"]
+            expires_in = response.json()["expires_in"]
             self.token_expires = datetime.now() + timedelta(seconds=expires_in)
             return self.token
         raise ValueError("Authentication failed")
 
     def refresh_access_token(self, refresh_token) -> str:
         """Refreshes the access token"""
-        headers = {
-            'Authorization': f'Basic {self.get_auth_header()}'
-        }
-        data = {
-            'grant_type': 'refresh_token',
-            'refresh_token': refresh_token
-        }
+        headers = {"Authorization": f"Basic {self.get_auth_header()}"}
+        data = {"grant_type": "refresh_token", "refresh_token": refresh_token}
         response = post(self.token_url, headers=headers, data=data, timeout=5)
         if response.status_code == 200:
-            self.token = response.json()['access_token']
-            expires_in = response.json()['expires_in']
+            self.token = response.json()["access_token"]
+            expires_in = response.json()["expires_in"]
             self.token_expires = datetime.now() + timedelta(seconds=expires_in)
             return self.token
         raise ValueError("Token refresh failed")
@@ -161,17 +162,17 @@ class SpotifyAPIManager:
     def get_access_token_header(self) -> dict:
         """Returns the access token header"""
         access_token = self.get_access_token()
-        access_token_header = {
-            'Authorization': f'Bearer {access_token}'
-        }
+        access_token_header = {"Authorization": f"Bearer {access_token}"}
         return access_token_header
 
-    def get_resource(self, endpoint: str, params: list = None, fields: list = None) -> dict:
+    def get_resource(
+        self, endpoint: str, params: list = None, fields: list = None
+    ) -> dict:
         """Returns the resource from the endpoint"""
         url = self.api_url + endpoint
         headers = self.get_access_token_header()
         if fields is not None:
-            headers['Accept'] = 'application/json'
+            headers["Accept"] = "application/json"
         response = client.get(url, headers=headers, params=params, data=None, timeout=5)
         if response.status_code == 200:
             if fields is not None:
@@ -201,38 +202,48 @@ class SpotifyAPI:
         endpoint = f"playlists/{playlist_id}"
         params = {}
         if fields is not None:
-            params['fields'] = fields
+            params["fields"] = fields
         return self.api_manager.get_resource(endpoint, params=params)
 
-    def playlist_items_batch(self, playlist_id: str, offset: int = None, fields: list = None) \
-            -> dict:
+    def playlist_items_batch(
+        self, playlist_id: str, offset: int = None, fields: list = None
+    ) -> dict:
         """Returns the items of the playlist with the given id, offset by some amount"""
         endpoint = f"playlists/{playlist_id}/tracks"
         params = {}
         if offset is not None:
-            params['offset'] = offset
+            params["offset"] = offset
         if fields is not None:
-            params['fields'] = fields
+            params["fields"] = fields
         return self.api_manager.get_resource(endpoint, params=params)
 
-    def playlist_items(self, playlist_id: str, fields: list = None, playlist_length: int = None) \
-            -> dict:
+    def playlist_items(
+        self, playlist_id: str, fields: list = None, playlist_length: int = None
+    ) -> dict:
         """Returns the items of the playlist with the given id"""
         if playlist_length is None:
-            playlist_length = self.playlist(playlist_id, fields=['tracks.total'])['tracks']['total']
+            playlist_length = self.playlist(playlist_id, fields=["tracks.total"])[
+                "tracks"
+            ]["total"]
         items = []
         for offset in range(0, playlist_length, 100):
-            items.extend(self.playlist_items_batch(playlist_id, offset, fields)['items'])
+            items.extend(
+                self.playlist_items_batch(playlist_id, offset, fields)["items"]
+            )
         return items
 
     def playlist_remove_all_occurrences_of_items(self, playlist_id: str, items) -> bool:
         """Removes all occurrences of the given items from the playlist"""
         for i in range(0, len(items), 100):
-            if not self.playlist_remove_all_occurrences_of_items_batch(playlist_id, items[i:i+100]):
+            if not self.playlist_remove_all_occurrences_of_items_batch(
+                playlist_id, items[i: i + 100]
+            ):
                 return False
         return True
 
-    def playlist_remove_all_occurrences_of_items_batch(self, playlist_id: str, items: list) -> bool:
+    def playlist_remove_all_occurrences_of_items_batch(
+        self, playlist_id: str, items: list
+    ) -> bool:
         """Removes all occurrences of the given items from the playlist"""
         endpoint = f"{self.api_manager.api_url}playlists/{playlist_id}/tracks"
         headers = {"Authorization": f"Bearer {self.api_manager.token}"}
@@ -243,14 +254,20 @@ class SpotifyAPI:
         print(f"Error {response.status_code} occurred: {response.text}")
         raise ValueError("Request failed")
 
-    def playlist_add_items(self, playlist_id: str, track_ids: list, position: int = None):
+    def playlist_add_items(
+        self, playlist_id: str, track_ids: list, position: int = None
+    ):
         """Adds the given items to the playlist in order"""
         for i in range(0, len(track_ids), 100):
-            if not self.playlist_add_items_batch(playlist_id, track_ids[i:i+100], position):
+            if not self.playlist_add_items_batch(
+                playlist_id, track_ids[i: i + 100], position
+            ):
                 return False
         return True
 
-    def playlist_add_items_batch(self, playlist_id: str, track_ids: list, position: int = None):
+    def playlist_add_items_batch(
+        self, playlist_id: str, track_ids: list, position: int = None
+    ):
         """Adds the given items to the playlist in order"""
         endpoint = f"{self.api_manager.api_url}playlists/{playlist_id}/tracks"
         headers = {"Authorization": f"Bearer {self.api_manager.token}"}
