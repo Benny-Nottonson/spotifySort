fa816c27da843897c99319d924084ce7128fc708
Benny <bennynottonson@gmail.com>
2023-12-21 12:26:15 -0800

    huge speedup and api fix

--- image_processing.py
+++ image_processing.py
@@ -9,25 +9,20 @@ from numpy import (
     array,
     bincount,
     count_nonzero,
+    asarray,
+    uint32,
+    zeros
 )
 from PIL import Image, ImageFilter
-from skimage.measure import label
+from skimage.measure import label, regionprops
 from spotify_api import public_get as client_get
 
 @cache
-def get_image_from_url(url: str, size: int, blur: int, quantized_level: int) -> Image:
+def get_image_from_url(url: str) -> Image:
     """Gets an image from a URL and converts it to rgb"""
     response_data = client_get(url, timeout=5)
     pil_image: Image = Image.open(BytesIO(response_data.content), mode="r").convert("RGB")
-    return process_image(pil_image, size, blur, quantized_level)
-
-def process_image(image: Image, size: int, blur: int, quantized_level: int) -> Image:
-    """Processes an image by blurring, resizing, and quantizing it"""
-    resized_image = image.resize((size, size), Image.Resampling.LANCZOS)
-    blurred_image = resized_image.filter(ImageFilter.GaussianBlur(blur))
-    if quantized_level > 0:
-        quantized_image = blurred_image.quantize(quantized_level)
-    return quantized_image
+    return pil_image
 
 
 def blob_extract(mac_image: ndarray) -> tuple[int, ndarray]:
@@ -42,23 +37,28 @@ def blob_extract(mac_image: ndarray) -> tuple[int, ndarray]:
 
 def ccv(image_url: str, size=32, blur=2, quantized_level=16) -> tuple:
     """Calculates the Color Coherence Vector of an image"""
-    image: Image = get_image_from_url(image_url, size, blur, quantized_level)
-    image_array = array(image)
-    size_threshold = round(0.01 * size * size)
-    n_blobs, blob = blob_extract(image_array)
-    table = [
-        [image_array[i][j], table[blob[i][j] - 1][1] + 1] if blob[i][j] != 0 else [0, 0]
-        for i in range(blob.shape[0])
-        for j in range(blob.shape[1])
-        for table in [[[0, 0] for _ in range(0, n_blobs)]]
-    ]
-    color_coherence_vector = [(0, 0) for _ in range(quantized_level)]
-    for color_index, blob_size in ((entry[0], entry[1]) for entry in table):
-        color_coherence_vector[color_index] = (
-            color_coherence_vector[color_index][0] + blob_size * (blob_size >= size_threshold),
-            color_coherence_vector[color_index][1] + blob_size * (blob_size < size_threshold),
+    image: Image = get_image_from_url(image_url)
+    image = asarray(
+            image.resize(
+                (size, size),
+                Image.LANCZOS,
+            )
+            .filter(ImageFilter.GaussianBlur(radius=blur))
+            .convert("P", palette=Image.ADAPTIVE, colors=quantized_level),
+            dtype=uint32,
         )
-    return tuple(color_coherence_vector)
+    size_threshold = round(0.01 * size * size)
+    blob = label(image, connectivity=2) + 1
+    blobs = regionprops(blob)
+    ccv = zeros((quantized_level, 2), dtype=uint32)
+    for b in blobs:
+        size = b.area
+        location = b.coords[0][0]
+        ccv[image[location][0]][
+            int(size <= size_threshold)
+        ] += size
+    ccv = tuple(tuple(x) for x in ccv)
+    return ccv
 
 
 @cache
